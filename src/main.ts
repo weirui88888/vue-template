1852e6d423da6ff061a779f26647852eeb4b8ef3
xdzi8b <ruiwei88888@163.com>
2024-05-30 15:44:49 +0800

    feat(pkg): install lint-staged

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import 'ant-design-vue/dist/reset.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+const a = 123
+
 import App from './App.vue'
 import router from './router'
 
